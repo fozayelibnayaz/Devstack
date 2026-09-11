9d3f0caf187b681119d8d19a1d07dc4b26f594a6
fozayelibnayaz <ibnayaz789@gmail.com>
2026-09-11 22:47:11 +0600

    fixing builds

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/Devstack/",
+  build: {
+    outDir: "docs",
+  },
   plugins: [react()],
   server: {
     host: true,
