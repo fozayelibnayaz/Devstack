cba3174f9afe9868bfb0d77e400188b5643bdc51
fozayelibnayaz <ibnayaz789@gmail.com>
2026-09-11 15:59:53 +0600

    added toast alerts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Suspense, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TechnologiesSection from "./components/TechnologiesSection";
@@ -11,15 +12,25 @@ function App() {
   const [stack, setStack] = useState([]);
 
   function addToStack(tech) {
-    if (stack.some((item) => item.id === tech.id)) return;
+    if (stack.some((item) => item.id === tech.id)) {
+      toast.warn(tech.name + " is already in your stack!");
+      return;
+    }
     setStack([...stack, tech]);
+    toast.success(tech.name + " added to your stack!");
   }
 
   function removeFromStack(tech) {
     setStack(stack.filter((item) => item.id !== tech.id));
+    toast.info(tech.name + " removed from your stack.");
   }
 
   function removeAll() {
+    if (stack.length === 0) {
+      toast.info("Your stack is already empty.");
+      return;
+    }
+    toast.info("Stack cleared - " + stack.length + " technologies removed.");
     setStack([]);
   }
 
@@ -38,6 +49,8 @@ function App() {
           />
         </Suspense>
       </main>
+
+      <ToastContainer position="top-right" autoClose={2500} pauseOnHover theme="light" />
     </div>
   );
 }
