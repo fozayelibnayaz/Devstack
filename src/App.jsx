a13dfb13a74071cdd4757259d0290d1077ade178
fozayelibnayaz <ibnayaz789@gmail.com>
2026-09-11 18:36:48 +0600

    fixed some issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,9 @@ import Hero from "./components/Hero";
 import TechnologiesSection from "./components/TechnologiesSection";
 import Footer from "./components/Footer";
 
-const technologiesPromise = fetch("/data/technologies.json")
+const technologiesPromise = fetch(import.meta.env.BASE_URL + "data/technologies.json")
   .then((response) => response.json())
   .then((data) => new Promise((resolve) => setTimeout(() => resolve(data), 600)));
-
 function App() {
   const [stack, setStack] = useState([]);
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: "/Devstack/",
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: true,
+  },
+});
